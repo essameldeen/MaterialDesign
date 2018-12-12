fe6ccf6d83cd1044487978f9712629f9309601f3
essameldeen <38116813+essameldeen@users.noreply.github.com>
2018-12-12 21:45:26 +0200

    navigation button view

--- app/src/main/java/com/example/essam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/essam/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.essam.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
@@ -21,6 +26,9 @@ import android.widget.Toast;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -39,6 +47,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     FloatingActionMenu actionMenu;
     FloatingActionButton edit, image;
+
+    AlertDialog dialog;
+    AlertDialog.Builder builder;
 
 
     @Override
@@ -148,6 +159,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         setTitle("Material Design");
 
+
+        builder = new AlertDialog.Builder(this)
+                .setTitle("Are You Sure ?")
+                .setMessage("wla wla wla wla wla wla wla wla wla wla wla ")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "YES", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "No", Toast.LENGTH_SHORT).show();
+
+                        dialog.dismiss();
+
+                    }
+                });
+
+        dialog = builder.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.textColor));
+        dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.textColor));
+
     }
 
 
@@ -233,16 +269,77 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void forgetPassword(View view) {
-        Toast.makeText(this, "FORGET FUNCTION", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, NavigationBar.class);
+        startActivity(intent);
     }
 
 
     public void imageFloating(View view) {
-        Toast.makeText(this, "Image Floating button", Toast.LENGTH_SHORT).show();
+        final ProgressDialog progressDialog;
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Downloading");
+        progressDialog.setMessage("Please Wait while your content Downloading ");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setMax(50);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        final Handler handler = new Handler();
+        final Timer timer = new Timer();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                if (i <= 50) {
+                    i += 2;
+                    progressDialog.setProgress(i);
+                } else {
+                    timer.cancel();
+                    i = 0;
+                    progressDialog.dismiss();
+                }
+            }
+        };
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+
+            }
+        }, 5000, 200);
+
     }
 
     public void editFloating(View view) {
-        Toast.makeText(this, "Edit Floating button", Toast.LENGTH_SHORT).show();
+        final String[] item = {"all", "some", "not Thing "};
+        AlertDialog alertDialog;
+        AlertDialog.Builder builder;
+        final String[] choise = {""};
+
+        builder = new AlertDialog.Builder(this, R.style.AlertDialog)
+                .setTitle("Select level of Change")
+                .setSingleChoiceItems(item, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        choise[0] = item[which];
+                    }
+                })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, choise[0], Toast.LENGTH_SHORT).show();
+
+                    }
+                }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "No Thanks", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        alertDialog = builder.show();
 
     }
+
 }
